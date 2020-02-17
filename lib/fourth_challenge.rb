e9ec78c57513ddd23ed77b8782e15343f25eaa2f
Janu Sung <janu.m.sung@gmail.com>
2020-02-17 01:34:25 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -27,7 +27,7 @@ def fourth_challenge
         {
           :name => "Steven",
           :age => "30",
-          :attitude "confused"
+          :attitude => "confused"
         }
         { 
           :name => "Nurse",
