4721bf36b5bccfb1152a192e1c5da391ec51dae0
Janu Sung <janu.m.sung@gmail.com>
2020-02-17 01:31:35 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -10,7 +10,11 @@ def fourth_challenge
           :age => "17",
           :attitude => "worried"
         },
-        "Mercutio" = 
+        { 
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        }
         }
         #your hashes here!
       ]
@@ -20,6 +24,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitude "confused"
+        }
+        { 
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }
         #your hashes here!
       ]
    }
